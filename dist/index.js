5cc368538ef42b2b2fe08cf69f8e0f895032d99a
Steven Zeiler <me@stevezeiler.com>
2019-05-10 13:35:42 -0400

    export getConnection

--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,6 @@ const actor_1 = require("./lib/actor");
 exports.Actor = actor_1.Actor;
 const logger_1 = require("./lib/logger");
 exports.log = logger_1.log;
+const amqp_1 = require("./lib/amqp");
+exports.getConnection = amqp_1.getConnection;
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -3,7 +3,10 @@ import { Actor } from './lib/actor';
 
 import { log } from './lib/logger';
 
+import { getConnection } from './lib/amqp';
+
 export {
   Actor,
-  log
+  log,
+  getConnection
 }
