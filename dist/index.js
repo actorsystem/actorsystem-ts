1c850387e4106b14b42f14b4a0ab3f8763f44710
Steven Zeiler <me@stevezeiler.com>
2019-08-14 16:00:29 -0400

    remove unused index files

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const actor_1 = require("./lib/actor");
-exports.Actor = actor_1.Actor;
-const logger_1 = require("./lib/logger");
-exports.log = logger_1.log;
-const amqp_1 = require("./lib/amqp");
-exports.getConnection = amqp_1.getConnection;
-//# sourceMappingURL=index.js.map
