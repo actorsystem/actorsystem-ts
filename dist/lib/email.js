8ad6bd12f3ab4f042d2b286ea147aa65bee7888c
Steven Zeiler <me@stevezeiler.com>
2021-10-11 16:54:24 +0200

    Update to amqplib 0.8

--- dist/lib/email.js
+++ dist/lib/email.js
@@ -1,13 +1,15 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.sendEmail = exports.send = void 0;
 require('dotenv').config();
 const requireAll = require("require-all");
 const AWS = require("aws-sdk");
@@ -91,6 +93,7 @@ function sendEmail(templateName, emailAddress, fromEmail, vars = {}) {
             Source: fromEmail,
             ReplyToAddresses: [
                 'steven@anypayinc.com',
+                /* more items */
             ],
         };
         // Create the promise and SES service object
--- dist/lib/logger.js
+++ dist/lib/logger.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.log = void 0;
 const winston = require("winston");
 const log = winston.createLogger({
     level: 'info',
