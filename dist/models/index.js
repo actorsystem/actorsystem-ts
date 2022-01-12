b5ab15737951f0255d75bef9156f1a2ed1d12ee3
Steven Zeiler <me@stevezeiler.com>
2022-01-12 18:02:14 +0100

    Manually copy models directory to dist

--- dist/models/index.js
+++ dist/models/index.js
@@ -1,34 +1,37 @@
 'use strict';
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
-let sequelize;
-if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], config);
-}
-else {
-    sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-fs
+//const env = process.env.NODE_ENV || 'development';
+//const config = require(__dirname + '/../config/config.json')[env];
+
+module.exports = function(url, config={dialect: 'postgresql'}) {
+
+  const db = {};
+
+  let sequelize = new Sequelize(url, config);
+
+  fs
     .readdirSync(__dirname)
     .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-})
+      return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    })
     .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-});
-Object.keys(db).forEach(modelName => {
+      const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+      db[model.name] = model;
+    });
+
+  Object.keys(db).forEach(modelName => {
     if (db[modelName].associate) {
-        db[modelName].associate(db);
+      db[modelName].associate(db);
     }
-});
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-console.log(db);
-module.exports = db;
-//# sourceMappingURL=index.js.map
+  });
+
+  db.sequelize = sequelize;
+  db.Sequelize = Sequelize;
+
+  return db
+
+}
